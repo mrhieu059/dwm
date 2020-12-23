b162e3fdcfdf956da89bec6d5b86709771274168
Hieu Cao <hieucao@protonmail.com>
2020-12-23 17:14:48 -0500

    Succeed transfer to Gentoo

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_gray1, "-sf", col_gray2, "-b", "-p", "Dango: " , "-i", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 #include "unfloat.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
